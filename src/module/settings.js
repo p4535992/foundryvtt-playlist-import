df98c6c4c759aa479d3e062557d8f3e6c5121677
p4535992 <tentimarco0@gmail.com>
2022-07-14 22:51:22 +0200

    u

--- src/module/settings.js
+++ src/module/settings.js
@@ -1,7 +1,6 @@
 import CONSTANTS from './constants.js';
 
 export const registerSettings = function () {
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'songs', {
     name: `${CONSTANTS.MODULE_NAME}.Songs`,
     hint: `${CONSTANTS.MODULE_NAME}.SongsHint`,
@@ -10,7 +9,6 @@ export const registerSettings = function () {
     default: {},
     type: Object,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'bucket', {
     name: `${CONSTANTS.MODULE_NAME}.BucketSelect`,
     hint: `${CONSTANTS.MODULE_NAME}.BucketSelectHint`,
@@ -19,7 +17,6 @@ export const registerSettings = function () {
     default: '',
     type: String,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'shouldRepeat', {
     name: `${CONSTANTS.MODULE_NAME}.ShouldRepeat`,
     hint: `${CONSTANTS.MODULE_NAME}.ShouldRepeatHint`,
@@ -28,7 +25,6 @@ export const registerSettings = function () {
     default: false,
     type: Boolean,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'shouldStream', {
     name: `${CONSTANTS.MODULE_NAME}.ShouldStream`,
     hint: `${CONSTANTS.MODULE_NAME}.ShouldStreamHint`,
@@ -37,7 +33,6 @@ export const registerSettings = function () {
     default: false,
     type: Boolean,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'folderDir', {
     name: `${CONSTANTS.MODULE_NAME}.FolderDir`,
     hint: `${CONSTANTS.MODULE_NAME}.FolderDirHint`,
@@ -47,7 +42,6 @@ export const registerSettings = function () {
     type: String,
     filePicker: true,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'logVolume', {
     name: `${CONSTANTS.MODULE_NAME}.LogVolume`,
     hint: `${CONSTANTS.MODULE_NAME}.LogVolumeHint`,
@@ -56,7 +50,6 @@ export const registerSettings = function () {
     default: '0.5',
     type: String,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'enableDuplicateChecking', {
     name: `${CONSTANTS.MODULE_NAME}.EnableDuplicate`,
     hint: `${CONSTANTS.MODULE_NAME}.EnableDuplicateHint`,
@@ -65,7 +58,6 @@ export const registerSettings = function () {
     default: true,
     type: Boolean,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'customRegexDelete', {
     name: `${CONSTANTS.MODULE_NAME}.CustomRegexDelete`,
     hint: `${CONSTANTS.MODULE_NAME}.CustomRegexDeleteHint`,
@@ -74,7 +66,6 @@ export const registerSettings = function () {
     default: '^\\d\\d+ *_*-* *',
     type: String,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'shouldOverridePlaylist', {
     name: `${CONSTANTS.MODULE_NAME}.ShouldOverridePlaylist`,
     hint: `${CONSTANTS.MODULE_NAME}.ShouldOverridePlaylistHint`,
@@ -85,7 +76,6 @@ export const registerSettings = function () {
   });
   let sources = new FilePicker().sources;
   let options = Object.keys(sources);
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'source', {
     name: game.i18n.localize(`${CONSTANTS.MODULE_NAME}.SelectSource`),
     hint: `${game.i18n.localize(`${CONSTANTS.MODULE_NAME}.SelectSourceHint`)} [${options}]`,
@@ -94,7 +84,6 @@ export const registerSettings = function () {
     default: 'data',
     type: String,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'shouldDeletePlaylist', {
     name: `${CONSTANTS.MODULE_NAME}.ShouldDeletePlaylist`,
     hint: `${CONSTANTS.MODULE_NAME}.ShouldDeletePlaylistHint`,
@@ -103,7 +92,6 @@ export const registerSettings = function () {
     default: false,
     type: Boolean,
   });
-
   game.settings.register(CONSTANTS.MODULE_NAME, 'maintainOriginalFolderName', {
     name: `${CONSTANTS.MODULE_NAME}.MaintainOriginalFolderName`,
     hint: `${CONSTANTS.MODULE_NAME}.MaintainOriginalFolderNameHint`,
